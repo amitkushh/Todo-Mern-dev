dedf3a0be5deeea7793a5557a9fbe952259dfb86
Amit Kushwaha <amitcoding56@gmail.com>
2025-02-20 17:15:28 +0530

    install neobrutalism ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
+import { Button } from "./components/ui/button.jsx";
+
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-4xl text-pink-700">hello</h1>
+      <div className="h-screen flex justify-center items-center gap-6">
+        <Button variant="reverse" size="lg">
+          Hello
+        </Button>
+        <Button variant="reverse" size="lg">
+          Click
+        </Button>
       </div>
     </>
   );
