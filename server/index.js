a97fdebad308ffaf014e5168de15b24ad1103aff
Amit Kushwaha <amitcoding56@gmail.com>
2025-02-20 15:37:38 +0530

    project create

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => {
+  res.send("welcome to Home Page");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
