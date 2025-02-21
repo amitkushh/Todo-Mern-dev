b44eb0d7d3b9d90c626fe11f2653138c5b0a8e74
Amit Kushwaha <amitcoding56@gmail.com>
2025-02-21 10:52:51 +0530

    hero section done

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Button } from "../ui/button";
+import { Link } from "react-router-dom";
 
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <div className="flex flex-col justify-center items-center gap-8 px-7 py-16">
+      <h1 className="text-5xl text-center font-semibold md:text-6xl md:max-w-[800px] leading-[110%] md:leading-[120%]">
+        Master Your Day with the Ultimate{" "}
+        <span className="text-main shadow-lg">To-Do List!</span>
+      </h1>
+      <p className="text-center md:max-w-[500px]">
+        Tired of forgetting tasks or feeling overwhelmed? Stay on top of
+        everything with a to-do list that works for you! Organize, prioritize,
+        and conquer your day—one task at a time.{" "}
+      </p>
+      <Link to="/sign-up">
+        <Button>Get Started</Button>
+      </Link>
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
