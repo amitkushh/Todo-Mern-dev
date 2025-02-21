4b53f951beaf084a7f0c88c0ba6ca3105881a77f
Amit Kushwaha <amitcoding56@gmail.com>
2025-02-21 13:43:05 +0530

    not found page add

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import Navbar from '../Navbar/Navbar'
-import Footer from '../Footer/Footer'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import { Outlet, useLocation } from "react-router-dom";
 
 function Layout() {
+  const location = useLocation();
+
+  const isNotFound =
+    location.pathname !== "/" &&
+    location.pathname !== "/home" &&
+    location.pathname !== "/sign-up" &&
+    location.pathname !== "/login";
+
   return (
     <>
-    <Navbar />
-    <Outlet />
-    <Footer />
+      {!isNotFound && <Navbar />}
+      <Outlet />
+      {!isNotFound && <Footer />}
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function PageNotFound() {
   return (
-    <div>PageNotFound</div>
-  )
+    <div className="h-screen flex flex-col gap-1 justify-center items-center">
+      <p className="text-xl font-bold">404</p>
+      <p className="text-xl">Page Not Found</p>
+      <p>
+        Go to{" "}
+        <Link to="/">
+          <span className="font-bold text-md text-main">Home page</span>
+        </Link>
+      </p>
+    </div>
+  );
 }
 
-export default PageNotFound
+export default PageNotFound;
