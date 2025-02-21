53c26808cdad336cf9267a3880cd140111ddee19
Amit Kushwaha <amitcoding56@gmail.com>
2025-02-21 11:25:38 +0530

    sign up, login ui done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="flex flex-col justify-center items-center px-7 gap-7 py-16 md:py-[90px]">
+      <h2 className="text-3xl font-semibold">Login</h2>
+      <form className="flex flex-col justify-center items-center gap-5 bg-main p-10 rounded-md">
+        <Input
+          className="w-[200px]"
+          type="email"
+          placeholder="Email"
+          required
+        />
+        <Input
+          className="w-[200px]"
+          type="password"
+          placeholder="Password"
+          required
+        />
+        <Button>Login</Button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <div className="flex flex-col justify-center items-center px-7 gap-7 py-16 md:py-[90px]">
+      <h2 className="text-3xl font-semibold">Sign Up</h2>
+      <form className="flex flex-col justify-center items-center gap-5 bg-main p-10 rounded-md">
+        <Input
+          className="w-[200px]"
+          type="text"
+          placeholder="Username"
+          required
+        />
+        <Input
+          className="w-[200px]"
+          type="email"
+          placeholder="Email"
+          required
+        />
+        <Input
+          className="w-[200px]"
+          type="password"
+          placeholder="Password"
+          required
+        />
+        <Button>Sign Up</Button>
+      </form>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
