5bb682874eb9acb4e8fa502a5e7e38cfee649867
Amit Kushwaha <amitcoding56@gmail.com>
2025-02-21 10:32:56 +0530

    navbar done

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Menubar, MenubarMenu, MenubarTrigger } from "@/components/ui/menubar";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="flex justify-center items-center py-7 px-7">
+      <Menubar>
+        <MenubarMenu>
+          <Link to="/">
+            <MenubarTrigger className="cursor-pointer">Home</MenubarTrigger>
+          </Link>
+        </MenubarMenu>
+        <MenubarMenu>
+          <Link to="/sign-up">
+            <MenubarTrigger className="cursor-pointer">Sign up</MenubarTrigger>
+          </Link>
+        </MenubarMenu>
+        <MenubarMenu>
+          <Link to="/login">
+            <MenubarTrigger className="cursor-pointer">Login</MenubarTrigger>
+          </Link>
+        </MenubarMenu>
+      </Menubar>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
