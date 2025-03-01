56359733f79aad6bb876f0a8b4e18a194e3e191c
Amit Kushwaha <amitcoding56@gmail.com>
2025-03-01 13:52:30 +0530

    cors add

--- server/index.js
+++ server/index.js
@@ -3,10 +3,20 @@ import dotenv from "dotenv";
 import connectDB from "./db/mongoose.js";
 import todoRouter from "./routes/todoRoute.js";
 import userRouter from "./routes/userRoute.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND,
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 connectDB();
 
